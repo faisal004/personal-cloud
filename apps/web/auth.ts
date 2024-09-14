5855f067e60173a3a95f218796279e21b227c477
faisal004 <faisalhusain1320@gmail.com>
2024-09-14 22:31:23 +0530

    fix : type  error fixed

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -2,9 +2,6 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    pages: {
-        error: "/error"
-    },
 
     providers: [GitHub],
 
