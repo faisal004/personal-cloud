a2f72a6be951694ddb7a7dfbdedbeee14a81dcfe
faisal004 <faisalhusain1320@gmail.com>
2024-09-15 14:18:12 +0530

    Revert "ok"
    This reverts commit 316e07d6e6785ac2a6b46de5df3220324c74acbc.

---
+++ packages/database/src/schema.ts
@@ -0,0 +1,28 @@
+import { integer, pgTable, serial, text, timestamp ,boolean} from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users_table', {
+  id: text('id').primaryKey().notNull(),
+  name: text('name').notNull(),
+  age: integer('age').notNull(),
+  email: text('email').notNull().unique(),
+
+});
+
+
+
+export const authenticators = pgTable('authenticators', {
+  id: text('id').primaryKey().notNull(), 
+  credentialID: text('credential_id').notNull().unique(), 
+  userId: text('user_id') 
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }), 
+  providerAccountId: text('provider_account_id').notNull(),
+  credentialPublicKey: text('credential_public_key').notNull(),
+  counter: integer('counter').notNull(),
+  credentialDeviceType: text('credential_device_type').notNull(),
+  credentialBackedUp: boolean('credential_backed_up').notNull(),
+  transports: text('transports').default(""),
+});
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type SelectUser = typeof usersTable.$inferSelect;
