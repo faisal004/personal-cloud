3c3e1009dccbe3649bd734655434a4e08acf5c22
faisal004 <faisalhusain1320@gmail.com>
2024-09-15 15:28:10 +0530

    added : trpc setup done

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 import { SessionProvider } from 'next-auth/react'
+import Provider from './_trpc/Provider'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -26,7 +27,10 @@ export default function RootLayout({
     <SessionProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <Provider>
           {children}
+
+          </Provider>
         </body>
       </html>
     </SessionProvider>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,16 @@
+import TodoList from "./_components/Todolist";
 
 
 export default function Home() {
   return (
-   <div className="bg-red-500 ">
+ 
+ <>
+  <div className="bg-red-500 ">
     hi
    </div>
+   <TodoList/>
+    </>
+ 
+
   );
 }
