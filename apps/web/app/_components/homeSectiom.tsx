71ec75bdbdd9d510de77f3abb46606aae2797f49
faisal004 <faisalhusain1320@gmail.com>
2024-09-16 20:18:00 +0530

    fixed : middleware fixed

--- apps/web/app/_components/homeSectiom.tsx
+++ apps/web/app/_components/homeSectiom.tsx
@@ -1,5 +1,10 @@
 'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 const HomeSection = () => {
+  const { status } = useSession()
+
   return (
     <div className="flex flex-col gap-1 items-center justify-center pt-[50px] md:pt-12">
       <div className="flex flex-col items-center justify-center  ">
@@ -18,9 +23,29 @@ const HomeSection = () => {
       </div>
       <div className="flex flex-col items-center justify-center mt-[80px] md:mt-[0px]">
         <div>
-          <button className="bg-black rounded-full font-light text-white px-10 py-2 text-2xl mt-7 tracking-wider">
-            Sign In
-          </button>
+          {status === 'authenticated' ? (
+            <>
+              {/* <button
+                onClick={() => signIn('passkey', { action: 'register' })}
+                className="bg-black rounded-full font-light text-white px-10 py-2 text-2xl mt-7 tracking-wider"
+              >
+                N
+              </button> */}
+              <button
+                onClick={() => signOut()}
+                className="bg-black rounded-full font-light text-white px-10 py-2 text-2xl mt-7 tracking-wider"
+              >
+                Sign Out
+              </button>
+            </>
+          ) : status === 'unauthenticated' ? (
+            <button
+              onClick={() => signIn('passkey')}
+              className="bg-black rounded-full font-light text-white px-10 py-2 text-2xl mt-7 tracking-wider"
+            >
+              Sign In
+            </button>
+          ) : null}
         </div>
         <p className="text-xl md:mt-[40px] mt-[20px] md:mx-[0] mx-[20px] mb-[20px] md:max-w-[478px] max-w-[350px] md:text-[40px] text-[24px] md:leading-[44px] leading-[28px] text-[#1d1d1f] font-semibold text-center">
           The best place for all your photos, files, notes, mail, and more.
--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -1,12 +1,32 @@
 import { db } from "@repo/db"
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthConfig } from "next-auth"
 import Passkey from "next-auth/providers/passkey"
 
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter:DrizzleAdapter(db),
+
+export const authConfig =
+{
+    adapter: DrizzleAdapter(db),
 
     providers: [Passkey],
     experimental: { enableWebAuthn: true },
-})
+    callbacks: {
+  
+        authorized({ auth, request: { nextUrl } }) {
+          const isLoggedIn = !!auth?.user
+          const paths = ["/error"]
+          const isProtected = paths.some((path) => nextUrl.pathname.startsWith(path))
+    
+          if (isProtected && !isLoggedIn) {
+            const redirectUrl = new URL("api/auth/signin", nextUrl.origin)
+            redirectUrl.searchParams.append("callbackUrl", nextUrl.href)
+            return Response.redirect(redirectUrl)
+          }
+          
+          return true
+        },
+      }
+    
+}satisfies NextAuthConfig
+export const { handlers, signIn, signOut, auth } = NextAuth(authConfig)
